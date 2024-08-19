723c93e555a3143631cdb4512c3fcb1f07aa2085
firaskudsy <firaskudsy@gmail.com>
2024-08-19 09:00:19 -0400

    feat: Add getUser function to userHelper module
    This commit adds the getUser function to the userHelper module. The getUser function is responsible for retrieving user data from the userDB module. It includes two test cases: one for successful retrieval of user data and another for handling errors when retrieving user data.

--- tests/userHelper.test.js
+++ tests/userHelper.test.js
@@ -27,4 +27,52 @@ describe('userHelper', () => {
       userHelper.getUsers(logTokens, callback);
     });
   });
+  describe('getUser', () => {
+    it('should call callback with data from userDB.getUser', (done) => {
+      const logTokens = 'someLogTokens';
+      const request = { id: 1 };
+      const data = { id: 1, name: 'John Doe' };
+
+      sinon.stub(userDB, 'getUser').resolves(data);
+
+      const callback = sinon.spy((result) => {
+        try {
+          expect(result).to.equal(data);
+          expect(callback.calledOnce).to.be.true;
+          expect(callback.calledWith(data)).to.be.true;
+          done();
+        } catch (error) {
+          done(error);
+        } finally {
+          userDB.getUser.restore();
+        }
+      });
+
+      userHelper.getUser(logTokens, request, callback);
+    });
+
+    it('should call callback with error if userDB.getUser fails', (done) => {
+      const logTokens = 'someLogTokens';
+      const request = { id: 1 };
+      const error = new Error('Database error');
+
+      sinon.stub(userDB, 'getUser').rejects(error);
+
+      const callback = sinon.spy((result) => {
+        try {
+          expect(result).to.equal(error);
+          expect(callback.calledOnce).to.be.true;
+          expect(callback.calledWith(error)).to.be.true;
+          done();
+        } catch (err) {
+          done(err);
+        } finally {
+          userDB.getUser.restore();
+        }
+      });
+
+      userHelper.getUser(logTokens, request, callback);
+    });
+  });
+
 });
